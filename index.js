f2a640ac4d7c27516029b1b523d3998114894ca1
enchanting-process-6535 <sajaamer86@gmail.com>
2019-09-14 17:33:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const append  =["Milo", "Otis", "Garfield"] ;
 const prepend  =["Milo", "Otis", "Garfield"] ;
 const removeLast  =["Milo", "Otis", "Garfield"] ;
 const removeFirst  =["Milo", "Otis", "Garfield"] ;
-append
-prepend
-removeLast
+append.push('')
+prepend.
+removeLast.
+removeFirst.
